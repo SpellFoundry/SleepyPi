dd37e8720b53ca1677b431100d7a5925bb66b305
Jon Watkins <jon.watkins@spellfoundry.com>
2015-04-12 15:33:12 +0100

    Two new examples added: ButtonOnOff2 and WakePeriodicallySimple

---
+++ examples/ButtonOnOff2/ButtonOnOff2.ino
@@ -0,0 +1,241 @@
+// 
+// Simple example showing how to set the Sleepy Pi to wake on button press
+// and then power up the Raspberry Pi. To switch the RPi off press the button
+// again. If the button is held dwon the Sleepy Pi will cut the power to the
+// RPi regardless of any handshaking.
+//
+
+// **** INCLUDES *****
+#include "SleepyPi.h"
+#include <Time.h>
+#include <LowPower.h>
+#include <DS1374RTC.h>
+#include <Wire.h>
+
+
+#define kBUTTON_POWEROFF_TIME_MS   2000
+#define kBUTTON_FORCEOFF_TIME_MS   8000
+
+
+// States
+typedef enum {
+  eWAIT = 0,
+  eBUTTON_PRESSED,
+  eBUTTON_WAIT_ON_RELEASE,
+  eBUTTON_HELD,
+  eBUTTON_RELEASED
+}eBUTTONSTATE;
+
+typedef enum {
+   ePI_OFF = 0,
+   ePI_BOOTING,
+   ePI_ON,
+   ePI_SHUTTING_DOWN
+}ePISTATE;
+
+const int LED_PIN = 13;
+
+volatile bool  buttonPressed = false;
+eBUTTONSTATE   buttonState = eWAIT;
+ePISTATE       pi_state = ePI_OFF;
+bool state = LOW;
+unsigned long  time, timePress;
+
+void button_isr()
+{
+    // A handler for the Button interrupt.
+    buttonPressed = true;
+}
+
+void alarm_isr()
+{
+    // A handler for the Alarm interrupt.
+}
+
+
+void setup()
+{
+  SleepyPi.simulationMode = false;  // Don't actually shutdown
+  
+  // Configure "Standard" LED pin
+  pinMode(LED_PIN, OUTPUT);		
+  digitalWrite(LED_PIN,LOW);		// Switch off LED
+
+  SleepyPi.enablePiPower(false);  
+  SleepyPi.enableExtPower(false);
+  
+  // Allow wake up triggered by button press
+  attachInterrupt(1, button_isr, LOW);       // button pin  
+}
+
+void loop() 
+{
+    bool pi_running;
+    unsigned long buttonTime;
+ 
+    // Enter power down state with ADC and BOD module disabled.
+    // Wake up when wake button is pressed.
+    // Once button is pressed stay awake - this allows the timer to keep running
+
+    switch(buttonState)
+    {
+        case eWAIT:
+             // Allow wake up alarm to trigger interrupt on falling edge.
+            attachInterrupt(0, alarm_isr, FALLING);    // Alarm pin
+            // Setup the Alarm Counter
+            SleepyPi.enableWakeupAlarm();
+            SleepyPi.setAlarm(5); // 2 Seconds or whatever.. 
+            
+            // Enter power down state with ADC and BOD module disabled.
+            // Wake up when wake up pin is low or Alarm fired.
+            SleepyPi.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+            // GO TO SLEEP....
+            // ....
+            // ....
+            // I'm awake !!!      
+            // What woke me up? Was it a button press or a scheduled wake?
+            // Lets check on button press
+           if(buttonPressed == false)
+           {           
+                // Was an alarm interrupt     
+                // Do some general housekeeping
+                // ...Check on our RPi
+                digitalWrite(LED_PIN,HIGH);    // Flash LE
+                pi_running = SleepyPi.checkPiStatus(false); 
+                switch(pi_state)
+                {            
+                  case ePI_BOOTING:
+                       // Check if we have finished booting
+                       if(pi_running == true)
+                       {
+                           // We have booted up!
+                           pi_state = ePI_ON;                  
+                       }
+                       else 
+                       {
+                           // Still not completed booting so lets carry on waiting
+                           pi_state = ePI_BOOTING;                   
+                       } 
+                       break;
+                  case ePI_ON:
+                       // Check if it is still on?
+                       if(pi_running == false)
+                       {
+                           // Shock horror! it's not running!!
+                           // Assume it has been manually shutdown, so lets cut the power
+                           // Force Pi Off               
+                           SleepyPi.enablePiPower(false);
+                           SleepyPi.enableExtPower(false);
+                           pi_state = ePI_OFF;                           
+                       }
+                       else 
+                       {
+                           // Still on - all's well - keep this state
+                           pi_state = ePI_ON;                   
+                       } 
+                       break;
+                  case ePI_SHUTTING_DOWN:
+                      // Is it still shutting down? 
+                       if(pi_running == false)
+                       {
+                           // Finished a shutdown
+                           // Force the Power Off               
+                           SleepyPi.enablePiPower(false);
+                           SleepyPi.enableExtPower(false);
+                           pi_state = ePI_OFF;                           
+                       }
+                       else 
+                       {
+                           // Still shutting down - keep this state
+                           pi_state = ePI_SHUTTING_DOWN;                   
+                       } 
+                      break;
+                  case ePI_OFF:             
+                  default:       // intentional drop thru
+                     // RPi off, so we'll continue to wait
+                     // for a button press to tell us to switch it on
+                     delay(10);
+                     pi_state = ePI_OFF;    
+                     break; 
+                }
+                buttonState = eWAIT; // Loop back around and go to sleep again
+                digitalWrite(LED_PIN,LOW);
+           }
+           else
+           {
+              buttonPressed = false;
+              // This was a button press so change the button state (and stay awake)
+              // Disable the alarm interrupt
+              detachInterrupt(0);              
+              // Disable external pin interrupt on wake up pin.
+              detachInterrupt(1);
+              buttonState = eBUTTON_PRESSED; 
+           }
+           break;
+        case eBUTTON_PRESSED:
+            buttonPressed = false;  
+            timePress = millis();    // Log Press time                
+            pi_running = SleepyPi.checkPiStatus(false);
+            if(pi_running == false)
+            {  
+                // Switch on the Pi
+                SleepyPi.enablePiPower(true);
+                SleepyPi.enableExtPower(true);
+                pi_state = ePI_BOOTING;   
+            }          
+            buttonState = eBUTTON_WAIT_ON_RELEASE;
+            digitalWrite(LED_PIN,HIGH);         
+            attachInterrupt(1, button_isr, HIGH);    // Will go high on release           
+            break;
+        case eBUTTON_WAIT_ON_RELEASE:
+            if(buttonPressed == true)
+            {
+                detachInterrupt(1); 
+                buttonPressed = false;                            
+                time = millis();     //  Log release time
+                buttonState = eBUTTON_RELEASED;
+            }
+            else
+            {
+                // Carry on waiting
+                buttonState = eBUTTON_WAIT_ON_RELEASE;  
+            }
+            break;
+        case eBUTTON_RELEASED:           
+            pi_running = SleepyPi.checkPiStatus(false);
+            if(pi_running == true)
+            {
+                // Check how long we have held button for
+                buttonTime = time - timePress;
+                if(buttonTime > kBUTTON_FORCEOFF_TIME_MS)
+                {
+                   // Force Pi Off               
+                   SleepyPi.enablePiPower(false);
+                   SleepyPi.enableExtPower(false);
+                   pi_state = ePI_OFF;          
+                } 
+                else if (buttonTime > kBUTTON_POWEROFF_TIME_MS)
+                {
+                    // Start a shutdown
+                    pi_state = ePI_SHUTTING_DOWN; 
+                    SleepyPi.piShutdown(false);      // true
+                    SleepyPi.enableExtPower(false);            
+                } 
+                else 
+                { 
+                     // Button not held off long - Do nothing
+                } 
+            } 
+            else 
+            {
+                // Pi not running  
+            }
+            digitalWrite(LED_PIN,LOW);            
+            attachInterrupt(1, button_isr, LOW);    // button pin
+            buttonState = eWAIT;        
+            break;
+        default:
+            break;
+    }
+}
+
---
+++ examples/WakePeriodicallySimple/WakePeriodicallySimple.ino
@@ -0,0 +1,254 @@
+// 
+// A Simple example showing how to set the RTC alarm pin to wake up the Arduino
+// and then power up the Raspberry Pi at set intervals.
+//
+// What it does
+// ============
+// It uses the Alarm function on the RTC which is a 24 bit down counter
+// clocked every second. Once the count reaches 0 - Boom! No, actually 
+// it triggers the alarm pin, then reloads and starts all over again.
+//
+// The Arduino, meantime, powers off the RPi and goes into a deep sleep
+// consuming virtually no power at all. When the Alarm goes off, it wakes up,
+// then wakes the RPi. The RPi thens boots up and performs the task that it
+// was awoken from it's slumber to perform. Once this is complete, ideally the 
+// RPi should issue a:
+//     sudo shutdown -h now
+// and shut itself down - safe in the knowledge that it's performed it's job
+// well.
+// All the while the Arduino is watching and monitoring. If it detects that the 
+// RPi has finished it's business, it cuts the last of the power and goes back into
+// a deep sleep once more, awaiting the next alarm.
+//
+// Options
+// ========
+// - It the "Configuration" section below setup the Interval and Time outs that
+//   you need.
+// - The actual time on the RTC doesn't really matter for this example as it is
+//   working on intervals. If you want the exact time you can sync it first 
+//   to the RPi: http://spellfoundry.com/sleepy-pi/accessing-real-time-clock-raspberry-pi/
+//   If you just want a close approximation, then use the code in the "Setup"
+//   section which will set the clock to the time that the code was compiled which
+//   will be about 10-20 seconds behind the actual time.
+// - The code is sprinkled with messages than can be removed or commented out 
+//   when running and reduce code size. You can only really see them via the development 
+//   cable: http://spellfoundry.com/products/sleepy-pi-programming-adapter/
+
+
+// +++++ Configuration ++++++++++
+// Adjust the values below to suit your needs
+// ...Interval - the time between RPi Power-ups
+#define TIME_INTERVAL_SECONDS    0
+#define TIME_INTERVAL_MINUTES    15    // i.e RPi powers up every 15 Mins   
+#define TIME_INTERVAL_HOURS      0
+#define TIME_INTERVAL_DAYS       0
+
+// ...Failsafe time. If your RPi hasn't
+// ...shutdown, we'll shut it down anyway
+// ...Note: the Failsafe should be less than your
+// ........ startup Interval!!
+#define RPI_POWER_TIMEOUT_MS     90000    // in ms - so this is 90 seconds
+
+// +++++ End Configuration +++++++
+
+// **** INCLUDES *****
+#include "SleepyPi.h"
+#include <Time.h>
+#include <LowPower.h>
+#include <DS1374RTC.h>
+#include <Wire.h>
+
+typedef enum {
+   ePI_OFF = 0,
+   ePI_BOOTING,
+   ePI_ON,
+   ePI_SHUTTING_DOWN,
+   ePI_SHUTDOWN
+}ePISTATE;
+
+ePISTATE  rpiState;
+const int LED_PIN = 13;
+const char *monthName[12] = {
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+};
+
+void alarm_isr()
+{
+    // Just a handler for the alarm interrupt.
+    // You could do something here
+
+}
+
+void setup()
+{ 
+  tmElements_t powerUpInterval, compileTime;
+   
+  // Configure "Standard" LED pin
+  pinMode(LED_PIN, OUTPUT);		
+  digitalWrite(LED_PIN,LOW);		// Switch off LED
+
+  SleepyPi.enablePiPower(false); 
+  SleepyPi.enableExtPower(false);
+  
+  rpiState = ePI_OFF;
+
+  // initialize serial communication: In Arduino IDE use "Serial Monitor"
+  Serial.begin(9600);
+  Serial.println("Starting, but I'm going to go to sleep for a while...");  
+  
+  // get the date and time the compiler was run
+  if (getDate(__DATE__,compileTime) && getTime(__TIME__,compileTime)) 
+  {
+    // and configure the RTC with this info
+    SleepyPi.setTime(compileTime);
+  }
+  
+  powerUpInterval.Second = TIME_INTERVAL_SECONDS; 
+  powerUpInterval.Minute = TIME_INTERVAL_MINUTES;
+  powerUpInterval.Hour   = TIME_INTERVAL_HOURS;
+  powerUpInterval.Day    = TIME_INTERVAL_DAYS;
+  Serial.print("Alarm Interval: ");
+  printTime(powerUpInterval,false);
+
+  SleepyPi.setAlarm(powerUpInterval);
+  SleepyPi.enableWakeupAlarm();
+  delay(1000);  
+}
+
+void loop() 
+{
+    unsigned long timeNowMs, timeStartMs;
+    tmElements_t  currentTime; 
+    bool pi_running;
+  
+    // Allow wake up alarm to trigger interrupt on falling edge.
+    attachInterrupt(0, alarm_isr, FALLING);		// Alarm pin
+    SleepyPi.ackAlarm();
+    
+    // Enter power down state with ADC and BOD module disabled.
+    // Wake up when wake up pin is low.
+    SleepyPi.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+    
+    // Disable external pin interrupt on wake up pin.
+    detachInterrupt(0); 
+    
+    SleepyPi.enablePiPower(true);   
+    Serial.print("I've Just woken up: ");
+    SleepyPi.readTime(currentTime);
+    printTime(currentTime,true); 
+    
+    digitalWrite(LED_PIN,HIGH);		// Switch on LED
+    delay(250);  
+    digitalWrite(LED_PIN,LOW);		// Switch off LED
+   
+    rpiState = ePI_BOOTING; 
+    
+    // The RPi is now awake. Wait for it to shutdown or
+    // Force it off after a set timeout.  
+    timeStartMs = timeNowMs = millis();
+    while (((timeNowMs - timeStartMs) < RPI_POWER_TIMEOUT_MS) && rpiState != ePI_SHUTDOWN)
+    {
+         pi_running = SleepyPi.checkPiStatus(false);
+         if(pi_running == true)
+         {
+            Serial.print("+");
+            rpiState = ePI_ON;
+            delay(200);      // milliseconds   
+         }
+         else
+         {
+           if(rpiState == ePI_BOOTING)
+           { 
+              Serial.print("."); 
+              delay(500);      // milliseconds   
+           }
+           else {
+              Serial.println(); 
+              Serial.println("RPi not running...");
+              rpiState = ePI_SHUTDOWN;               
+           }
+         }
+         timeNowMs = millis();
+    }
+    // Did a timeout occur?
+    if((timeNowMs - timeStartMs) >= RPI_POWER_TIMEOUT_MS)
+    {
+       Serial.println(); 
+       Serial.print("TimeOut! At: ");
+       if (SleepyPi.readTime(currentTime)) 
+       {
+          printTime(currentTime,false); 
+       }
+       // Manually Shutdown the Rpi
+       SleepyPi.piShutdown(true);      
+       SleepyPi.enableExtPower(false);
+       rpiState = ePI_OFF; 
+            
+    }
+    else
+    {
+       Serial.println(); 
+       Serial.println("RPi Shutdown, I'm going back to sleep..."); 
+       SleepyPi.piShutdown(true);      
+       SleepyPi.enableExtPower(false);
+       rpiState = ePI_OFF;       
+    } 
+   
+}
+
+
+
+bool printTime(tmElements_t tm, bool printDate)
+{
+    print2digits(tm.Hour);
+    Serial.write(':');
+    print2digits(tm.Minute);
+    Serial.write(':');
+    print2digits(tm.Second);
+    if(printDate == true)
+    {
+      Serial.print(", Date (D/M/Y) = ");
+      Serial.print(tm.Day);
+      Serial.write('/');
+      Serial.print(tm.Month);
+      Serial.write('/');
+      Serial.print(tmYearToCalendar(tm.Year));
+    }
+    Serial.println();   
+}
+
+bool getTime(const char *str, tmElements_t &time)
+{
+  int Hour, Min, Sec;
+
+  if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
+  time.Hour = Hour;
+  time.Minute = Min;
+  time.Second = Sec;
+  return true;
+}
+
+bool getDate(const char *str, tmElements_t &time)
+{
+  char Month[12];
+  int Day, Year;
+  uint8_t monthIndex;
+
+  if (sscanf(str, "%s %d %d", Month, &Day, &Year) != 3) return false;
+  for (monthIndex = 0; monthIndex < 12; monthIndex++) {
+    if (strcmp(Month, monthName[monthIndex]) == 0) break;
+  }
+  if (monthIndex >= 12) return false;
+  time.Day = Day;
+  time.Month = monthIndex + 1;
+  time.Year = CalendarYrToTm(Year);
+  return true;
+}
+
+void print2digits(int number) {
+  if (number >= 0 && number < 10) {
+    Serial.write('0');
+  }
+  Serial.print(number);
+}
